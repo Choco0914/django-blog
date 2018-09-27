3f035188f3f50b57ac93876e16edd511cc381f30
Choco0914 <heojeongho1992@gmail.com>
2018-09-27 09:23:07 +0900

    feet: Blog 프로젝트 모델 정의
    주제관련 모델을 정의 다.

--- my_blog/models.py
+++ my_blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """Topic은 사용자가 블로그에서 다루는 주제"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        """모델에 관한 정보를 문자열 형태로 반환한다."""
+        return self.text
