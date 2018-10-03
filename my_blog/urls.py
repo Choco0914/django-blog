953c5649fe882d92c1e222b619f57ae075f2f3d1
Choco0914 <heojeongho1992@gmail.com>
2018-10-03 15:29:24 +0900

    feet: 주제에 따른 개별 페이지 생성
    주제에 맞게 id값을 주어 url설정하고
    view함수를 편집
    template 설정

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -6,5 +6,11 @@ from . import views
 
 urlpatterns =[
     # Homepage
-    re_path(r'$', views.index, name = 'index')
+    re_path(r'^$', views.index, name = 'index'),
+
+    #Topic page
+    re_path(r'^topics/$', views.topics, name='topics'),
+
+    #주제 하나에 대한 세부사항 페이지
+    re_path(r'^topics/(?:page-(?P<topic_id>\d+)/)?$', views.topic, name='topic' ),
 ]
--- my_blog/views.py
+++ my_blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """블로그 홈페이지"""
     return render(request, 'my_blog/index.html')
+
+def topics(request):
+    """주제를 나타내는 page"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'my_blog/topics.html', context)
+
+def topic(request, topic_id):
+    """주제 하나와 연결된 모든 내용을 표시한다"""
+    topic = Topic.objects.get(id=topic_id)
+    contents = topic.content_set.order_by('-date_added')
+    context = {'topic':topic, 'contents': contents}
+    return render(request, 'my_blog/topic.html', context)
