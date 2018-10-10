92ee28434d591437c7d6ebe54318d680c258d8bc
Choco0914 <heojeongho1992@gmail.com>
2018-10-10 14:48:03 +0900

    admin css settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
 
@@ -9,4 +11,4 @@ urlpatterns = [
 
     # users app url
     re_path(r'^users/', include(('users.urls', 'users'), namespace=None)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
