a55e58e237bc63a3fb4c2f93a644704cf0f04ef1
Choco0914 <heojeongho1992@gmail.com>
2018-10-10 17:44:13 +0900

    ready for heroku

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
@@ -11,4 +9,4 @@ urlpatterns = [
 
     # users app url
     re_path(r'^users/', include(('users.urls', 'users'), namespace=None)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
