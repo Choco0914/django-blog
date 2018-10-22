4e68a909b04dbd1734dda8960ad95aae016d1a1e
Choco0914 <heojeongho1992@gmail.com>
2018-10-22 10:39:11 +0900

    pythonanywhere

--- users/models.py
+++ users/models.py
@@ -1,7 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, related_name='profile', on_delete = models.CASCADE) #1 to 1 link with Django User
-    activation_key = models.CharField(max_length=40)
-    key_expires = models.DateTimeField()
